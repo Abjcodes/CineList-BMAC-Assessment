e92b506a73566c1c4703953096e8f7c34d93b3fc
Abjcodes <abijithmv01@gmail.com>
2025-05-30 19:30:10 +0530

    Initial Commit

---
+++ CineList/CineListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineListApp.swift
+//  CineList
+//
+//  Created by Abijith Vasanthakumar on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CineListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineListTests/CineListTests.swift
@@ -0,0 +1,17 @@
+//
+//  CineListTests.swift
+//  CineListTests
+//
+//  Created by Abijith Vasanthakumar on 30/05/25.
+//
+
+import Testing
+@testable import CineList
+
+struct CineListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
